d091d16ff44414a4208b6c19d15f9b78eb3b28c9
Konstadinos Angelis <angeliskonstadinos@gmail.com>
2019-11-12 08:12:30 -0800

    finished stretch

--- index.js
+++ index.js
@@ -252,8 +252,16 @@ function getOlderCars(inventory, year) {
  * made by either `Audi` or `Mercedes-Benz` or `Volkswagen` or `BMW`,
  * in the same order as they appear in the original inventory.
 */
-function getGermanCars(/* code here */) {
-  /* code here */
+function getGermanCars(inventory){
+  let germanCars = [];
+
+  for(let i = 0; i < inventory.length; i++){
+
+    if(inventory[i].car_make == "Audi" || inventory[i].car_make == "Mercedes-Benz" || inventory[i].car_make == "Volkswagen" || inventory[i].car_make == "BMW"){
+      germanCars.push(inventory[i]);
+    }
+  }
+  return germanCars;
 }
 
 /**
@@ -274,9 +282,9 @@ function getGermanCars(/* code here */) {
  *   return num * 2
  * }
 */
-const sum = null; // code here!
-const addFive = null; // code here!
-const argTimesTwo = null; // code here!
+const sum = () => {null}; // code here!
+const addFive = () => null; // code here!
+const argTimesTwo = () => null; // code here!
 
 /**
  * ### Challenge `carMaker`
@@ -291,8 +299,12 @@ const argTimesTwo = null; // code here!
  *         (1) causes the odometer in the object to be increased by the distance,
  *         (2) returns the updated value of the `odometer`.
 */
-function carMaker(/* code here */) {
-  /* code here */
+function carMaker(number) {
+  const object = {
+    odometer : number,
+    drive: function(distance){return object.odometer += distance},
+  }
+  return object;
 }
 
 /// ////// END OF CHALLENGE /////////
